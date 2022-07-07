6876b96586cfa7351403bd785f74795effce144f
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-07 15:51:55 +0530

    changes

--- S3/day3/src/App.js
+++ S3/day3/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Stopwatch from "./components/Stopwatch";
+import Stpwtch from "./components/Stpwtch";
 
 function App() {
   return (
     <div className="App">
       <Stopwatch />
+      <Stpwtch />
     </div>
   );
 }
