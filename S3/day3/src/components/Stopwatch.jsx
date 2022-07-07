eb8033fcfc44165a38b5d89c3bc76a823ec457fb
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-07 15:33:39 +0530

    ch

---
+++ S3/day3/src/components/Stopwatch.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useRef } from "react";
+
+const Stopwatch = () => {
+  const timerId = useRef();
+  const [time, setTime] = useState(0);
+
+  const start = () => {
+    if (!timerId.current) {
+      timerId.current = setInterval(() => {
+        setTime((time) => time + 1);
+      }, 1000);
+    }
+  };
+
+  const pause = () => {
+    clearInterval(timerId.current);
+    timerId.current = null;
+  };
+
+  const reset = () => {
+    clearInterval(timerId.current);
+    timerId.current = null;
+    setTime(0);
+  };
+
+  return (
+    <div>
+      <h1>{time}</h1>
+      <div>
+        <button onClick={start} disabled={time > 0}>
+          Start
+        </button>
+        <button onClick={pause} disabled={!timerId.current}>
+          Pause
+        </button>
+        <button onClick={reset} disabled={!timerId.current}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
