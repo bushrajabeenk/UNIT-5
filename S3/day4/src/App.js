2a2a01075bbd4ec1e357113af65e33b1770df9b0
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-09 21:03:08 +0530

    ch

--- S3/day4/src/App.js
+++ S3/day4/src/App.js
@@ -1,14 +1,31 @@
+import { useMemo, useState } from "react";
 import "./App.css";
-import Counter from "./components/Counter";
-import useCounter from "./hooks/useCounter";
+// import Counter from "./components/Counter";
+// import useCounter from "./hooks/useCounter";
 
 function App() {
-  const { count: c1, increment: i1, decrement: d1 } = useCounter(0);
-  const { count: c2, increment: i2, decrement: d2 } = useCounter(0);
+  // const { count: c1, increment: i1, decrement: d1 } = useCounter(0);
+  // const { count: c2, increment: i2, decrement: d2 } = useCounter(0);
+
+  const [ans, setAns] = useState();
+  const [val, setVal] = useState(0);
+
+  const fibo = (n) => {
+    if (n <= 1) return n;
+    return fibo(n - 1) + fibo(n - 2);
+  };
+
+  const memoisedValue = useMemo(() => fibo(val), [val]);
+
+  const handleClick = () => {
+    let res = memoisedValue;
+    console.log(res);
+    setAns(res);
+  };
 
   return (
     <div className="App">
-      <div>
+      {/* <div>
         <Counter
           label="Counter X"
           count={c1}
@@ -23,6 +40,15 @@ function App() {
           increment={() => i2(10)}
           decrement={() => d2(20)}
         />
+      </div> */}
+      <h2>{ans}</h2>
+      <div>
+        <input
+          type="number"
+          value={val}
+          onChange={(e) => setVal(e.target.value)}
+        />
+        <button onClick={handleClick}>Calculate Fibonacci</button>
       </div>
     </div>
   );
