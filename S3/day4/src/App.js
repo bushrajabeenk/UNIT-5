c668fc5e63632b05dea32d17bcf1d8792a263416
bushrajabeenk <74173626+bushrajabeenk@users.noreply.github.com>
2022-07-09 15:46:24 +0530

    ch

--- S3/day4/src/App.js
+++ S3/day4/src/App.js
@@ -3,16 +3,27 @@ import Counter from "./components/Counter";
 import useCounter from "./hooks/useCounter";
 
 function App() {
-  const { count, increment, decrement } = useCounter(0);
+  const { count: c1, increment: i1, decrement: d1 } = useCounter(0);
+  const { count: c2, increment: i2, decrement: d2 } = useCounter(0);
 
   return (
     <div className="App">
-      <Counter
-        label="Counter X"
-        count={count}
-        increment={() => increment(1)}
-        decrement={() => decrement(1)}
-      />
+      <div>
+        <Counter
+          label="Counter X"
+          count={c1}
+          increment={() => i1(1)}
+          decrement={() => d1(1)}
+        />
+      </div>
+      <div>
+        <Counter
+          label="Counter Y"
+          count={c2}
+          increment={() => i2(10)}
+          decrement={() => d2(20)}
+        />
+      </div>
     </div>
   );
 }
